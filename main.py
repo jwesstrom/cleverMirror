17243cd22fd601f896abd324403111e70acb2810
jwesstrom <jwesstrom@gmail.com>
2016-07-17 14:38:29 +0200

    playing around with ellipses

--- main.py
+++ main.py
@@ -4,23 +4,27 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 import random
 from kivy.clock import Clock
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty
 from webScrape import yrScrape
 
 
 class MirrorWindow(Widget):
     dayPrint = ['Sön', 'Mån', 'Tis', 'Ons', 'Tors', 'Fre', 'Lör']
+    seconds = NumericProperty(0)
     time = StringProperty('')
     day = StringProperty('')
     date = StringProperty('')
     weather1 = StringProperty('')
     weather2 = StringProperty('')
     weather3 = StringProperty('')
+    #seconds = StringProperty('')
 
     def update(self, dt):
         self.time = datetime.datetime.today().strftime("%H:%M")
         self.day = self.dayPrint[int(datetime.date.today().strftime('%w'))]
         self.date = datetime.date.today().strftime('%y%m%d')
+        #self.seconds = str (( int (datetime.datetime.today().strftime('%f')) / 1000 ) )
+        self.seconds = ( int (datetime.datetime.today().strftime('%f')) / 1000 )
         # for i in range(3):
         #     try:
         #         self.weatheri = yrScrape(i).getWeather()
@@ -42,7 +46,7 @@ class MirrorWindow(Widget):
 class MirrorApp(App):
     def build(self):
         mirrorWindow = MirrorWindow()
-        Clock.schedule_interval(mirrorWindow.update, 1)
+        Clock.schedule_interval(mirrorWindow.update, 0.0001)
         return mirrorWindow
 
 
