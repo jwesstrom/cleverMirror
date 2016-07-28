595d9c76527e59a54e991ebe49efadac6c6b2bf0
jwesstrom <jwesstrom@gmail.com>
2016-07-28 21:11:53 +0200

    blahblah

--- testing/kivyTester/gfx_v003.py
+++ testing/kivyTester/gfx_v003.py
@@ -7,21 +7,47 @@ from kivy.uix.label import Label
 
 class dataVariables(Widget):
     def __init__(self):
-        self.subwayA = 15.0
+        self.subwayA = [0.0, 15.0]
     def update(self, dt):
-        self.subwayA = self.subwayA - 1
-        #print self.subwayA
+        if self.subwayA[0] < self.subwayA[1]:
+            self.subwayA[0] = self.subwayA[0] + 1.0
+        else:
+            self.subwayA[0] = 0.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class timerWidget(Widget):
     def __init__(self, timeleft, **kwargs):
         # make sure we aren't overriding any important functionality
         super(timerWidget, self).__init__(**kwargs)
-        self.timeleft = timeleft
+        self.timeleft = timeleft[:]
+
         self.groupList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         self.angleCounter = 0.0
         self.pieSlices = 5.0
         self.currentPieSlice = 0.0
         self.id = 'counter'
+
+        self.rgb = [0.0,1.0,1.0]
+        self.rgbCheck = [True, True, True]
+
+
 
     def resetCanvas(self):
         # print 'reset'
@@ -30,37 +56,57 @@ class timerWidget(Widget):
         self.canvas.clear()
         with self.canvas:
 
-            Color(0., 1., 0)
+            Color(0.0, 1.0, 0.0)
             Ellipse(pos=(350, 350), size=(200, 200), group='a', angle_start=0, angle_end = 10)
 
         self.angleCounter = 1.0
+        self.timeleft[0] = 0.0
+        self.rgb = [0.0,1.0,1.0]
+        self.rgbCheck = [True, True, True]
 
     def createEllipse(self):
-        self.currentPieSlice = self.currentPieSlice + 1
-        if self.currentPieSlice < self.pieSlices/2:
-            red = (1.0/(self.pieSlices+1) * (self.currentPieSlice *2 ))
-            green = 1.0
-        else:
-            red = 1.0
-            green = 1-(1.0/(self.pieSlices/2)*self.currentPieSlice)+1
-        # print 'red ' + str(red) + '_' + 'green ' + str(green)
+        self.currentPieSlice = self.currentPieSlice + 1.0
+        if self.currentPieSlice <= self.pieSlices/3.0:
+            print 'one'
+            if self.rgbCheck[2] == True:
+                self.rgbCheck[2] = False
+            else:
+                self.rgb[2] = self.rgb[2] - ((1.0) / (self.pieSlices/3.0))
+        elif self.currentPieSlice > self.pieSlices/3.0 and self.currentPieSlice <= self.pieSlices/3.0*2:
+            print 'two '
+            self.rgb[2] = 0.0
+            #self.rgb[1] = self.rgb[1] - ((1.0) / (self.pieSlices/3.0))
+            self.rgb[0] = self.rgb[0] + ((1.0) / (self.pieSlices/3.0))
+        elif self.currentPieSlice >= self.pieSlices/3.0*2:
+            print 'three '
+            self.rgb[2] = 0.0
+            self.rgb[0] = 1.0
+            self.rgb[1] = self.rgb[1] - ((1.0) / (self.pieSlices/3.0))
+                #self.rgb[0] = self.rgb[0] + ((1.0) / (self.pieSlices/3.0))
+
+        print self.rgb
         with self.canvas:
-            Color(red, green, 0)
+            Color(self.rgb[0], self.rgb[1], self.rgb[2])
             Ellipse(pos=(350, 350), size=(200, 200), group=self.groupList[int(self.currentPieSlice)], angle_start=self.angleCounter, angle_end=self.angleCounter)
-
-
 
     def update(self, dt):
         # print 'count' + str(self.angleCounter)
         # print 'check' + str(self.currentPieSlice)
-        print 360.0 / 60.0 * self.timeleft
+        #print 360.0 / 60.0 * self.timeleft
+        #360/self.timeleft[1]*60
+        #self.timeleft[0] = self.timeleft[0] + 1/60
+
+        self.timeleft[0] = (self.timeleft[0]) + (360/self.timeleft[1]/60)
 
         if self.currentPieSlice > self.pieSlices:
             self.resetCanvas()
         elif self.angleCounter == 0:
             self.resetCanvas()
         else:
-            self.angleCounter = self.angleCounter + 1
+            #self.angleCounter = self.angleCounter + 1
+            #self.angleCounter = 360/self.timeleft[1]*self.timeleft[0]
+            self.angleCounter = self.timeleft[0]
+            #print self.angleCounter
         currentGroup = self.groupList[int(self.currentPieSlice)]
         for i in self.canvas.get_group(currentGroup):
             if 'Ellipse' in str(type(i)):
@@ -78,8 +124,18 @@ class timerWidget(Widget):
             Ellipse(pos=(360, 360), size=(180, 180), group='qz', angle_start=0, angle_end=360)
 
     def updateTimer(self, timeleft, dt):
-        self.timeleft = timeleft
-        print 'bajsbajsbajsbajs ' + str(self.timeleft)
+        self.timeleft[0] = 360/timeleft[1]*timeleft[0]
+
+
+
+
+
+
+
+
+
+
+
 
 
 class clockText(Widget):
@@ -92,6 +148,16 @@ class clockText(Widget):
     def update(self, timeleft, dt):
         for i in self.children:
             i.text=str(timeleft)
+
+
+
+
+
+
+
+
+
+
 
 class windowWidget(Widget):
     def __init__(self, timeleft, **kwargs):
@@ -115,28 +181,17 @@ class windowWidget(Widget):
     def updateTimers(self, dt):
         for i in self.children:
             if i.id == 'counter':
-                i.update
-                (self.timeleft.subwayA,dt)
-
-
+                i.updateTimer(self.timeleft.subwayA, dt)
 
 
 
 class GfxApp(App):
     def build(self):
-        # gWindow = timerWidget()
-        # Clock.schedule_interval(gWindow.update, 0.001)
-        # clockText()
-        # return gWindow
-        #return windowWidget()
         data = dataVariables()
-        #Clock.schedule_interval(lambda dt: data.update, 1)
-        # Clock.schedule_interval(data.update, 1)
         Clock.schedule_interval(data.update, 1)
         gWindow = windowWidget(data)
         Clock.schedule_interval(gWindow.update, 1/60)
-        Clock.schedule_interval(gWindow.updateTimers, 5)
-        # Clock.schedule_interval(gWindow.updateSubway, 1)
+        Clock.schedule_interval(gWindow.updateTimers, 3)
         return gWindow
 
 
